4d1d62f8f8be5cf7e03b59e470bf418e7ba9fbca
joshua-disney <joshua.a.disney@gmail.com>
2019-07-12 10:52:42 -0700

    update backendURL to production site

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,11 +1,11 @@
 export const backendUrl =
-  process.env.NODE_ENV === 'development' &&
-  process.env.REACT_APP_TESTING_LOCAL_BACKEND === 'yespls'
-    ? 'http://localhost:5000'
-    // : 'https://halg-backend.herokuapp.com'; This was the original URL 
-    : 'https://lambdaappstore2.herokuapp.com/';
+  process.env.NODE_ENV === "development" &&
+  process.env.REACT_APP_TESTING_LOCAL_BACKEND === "yespls"
+    ? "http://localhost:5000"
+    : // : 'https://halg-backend.herokuapp.com'; This was the original URL
+      "https://lambdaappstore.herokuapp.com/";
 export const frontendUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    // : 'https://hirelambdastudents.com'; This was the original URL 
-    : 'https://angry-hermann-981c04.netlify.com/';
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : // : 'https://hirelambdastudents.com'; This was the original URL
+      "https://angry-hermann-981c04.netlify.com/";
