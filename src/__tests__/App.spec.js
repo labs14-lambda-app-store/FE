913dfb536e0d67de5d750ce72e15e2bbae6cf7dc
Kayla <6lackmacaroon@gmail.com>
2019-07-12 10:38:30 -0600

    debugging basic jest test failing - new error - jest encountered an unexpected token

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/react/cleanup-after-each';
 import { render } from '@testing-library/react';
 import App from '../App.js';
